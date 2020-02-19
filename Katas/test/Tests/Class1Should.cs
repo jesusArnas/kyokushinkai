7b1f25e9efcc0b098e2cc7b04e2451fd54a088be
Jesus Arnas Iñigo <jarnas@plainconcepts.com>
2020-02-19 18:18:09 +0100

    First commit with initial test

---
+++ Katas/test/Tests/Class1Should.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class Class1Should
+    {
+        [Fact]
+        public void RunFirstTest()
+        {
+            var myClass = new Class1();
+            myClass.Should().NotBeNull();
+        }
+    }
+}
